ed2333d2c6b480e45b6a72721e08dd623d29cce9
Abhishek agarwal <abhishekagarwal151@gmail.com>
2023-11-21 16:49:07 +0530

    emoji picker added ,emoji message to be displayed

--- src/components/EmojiMenu.jsx
+++ src/components/EmojiMenu.jsx
@@ -4,7 +4,7 @@ export default function EmojiMenu({isSelfMessage}){
     return (
 
     <div className={`absolute z-50  top-full translate-y-2 ${isSelfMessage ? "left-0":"right-0"}`} >
-        <unicode-emoji-picker filtersPosition="top"></unicode-emoji-picker>
+        <unicode-emoji-picker ></unicode-emoji-picker>
     </div>
     )
 }
